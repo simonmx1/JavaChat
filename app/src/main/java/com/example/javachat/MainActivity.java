d179623e0a1e7f5ce3081c7873b829bc2b5c4ceb
Simon <simon@muscatello.bz.it>
2019-02-21 15:19:00 +0100

    Besser gemocht
    Signed-off-by: Simon <simon@muscatello.bz.it>

--- app/src/main/java/com/example/javachat/MainActivity.java
+++ app/src/main/java/com/example/javachat/MainActivity.java
@@ -3,11 +3,18 @@ package com.example.javachat;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import static java.lang.Thread.sleep;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try {
+            sleep(30);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
