68923ae562be5e0556acadd4bcdac1e63ddab2e9
simon_mx1 <simon@muscatello.bz.it>
2019-03-09 21:13:35 +0100

    Chat Client drin

--- app/src/main/java/com/example/javachat/ChatClientThread.java
+++ app/src/main/java/com/example/javachat/ChatClientThread.java
@@ -7,17 +7,28 @@ import java.net.SocketException;
 public class ChatClientThread extends Thread
 {
 	private BufferedReader in = null;
-	
+
+	//Ausgabefeld:
+	private out area;
+
 	public ChatClientThread(BufferedReader in) {
 		this.in = in;
 	}
-	
+
 	@Override
 	public void run() {
 		try {
 			while (true) {
 				String line = in.readLine();
 				System.out.println(line);
+				if (line.contains("smile")) {
+					area.setText(area.getText() + ":) \n");
+					scrollPane.getVerticalScrollBar().setValue(scrollPane.getVerticalScrollBar().getMaximum());
+				} else {
+					area.setText(area.getText() + line + "\n");
+					scrollPane.getVerticalScrollBar().setValue(scrollPane.getVerticalScrollBar().getMaximum());
+				}
+				// System.out.println(line);
 			}
 		} catch (SocketException e) {
 			System.out.println("Connection to ChatServer lost, ignore exception");
@@ -26,3 +37,4 @@ public class ChatClientThread extends Thread
 		}
 	}
 }
+
