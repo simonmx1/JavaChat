70a29910c5a6dc5486c6af0404a1797ab4409969
simon_mx1 <simon@muscatello.bz.it>
2019-03-11 11:29:33 +0100

    Besser gemocht
    Signed-off-by: Simon <simon@muscatello.bz.it>

--- app/src/main/java/com/example/javachat/MainActivity.java
+++ app/src/main/java/com/example/javachat/MainActivity.java
@@ -1,22 +1,11 @@
 package com.example.javachat;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.net.Socket;
-
-import static java.lang.Thread.sleep;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,38 +27,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-        /*
-        Socket client = null;
-        try {
-            client = new Socket("localhost", PORT);
-            BufferedReader in =
-                    new BufferedReader( new InputStreamReader(client.getInputStream()));
-            PrintStream out = new PrintStream(client.getOutputStream());
-            BufferedReader consoleIn =
-                    new BufferedReader(new InputStreamReader(System.in));
-
-            // sending the name of the client to the server
-            out.println("Client");
-
-            new ChatClientThread(in).start();
-
-            while (true) {
-                String line = consoleIn.readLine();
-                if (line == null)
-                    // pressed [Ctrl]+Z to sign out
-                    break;
-                out.println(line);
-            }
-        } catch (IOException e) {
-            System.out.println(e.getClass().getName() + ": " + e.getMessage());
-        } finally {
-            try { client.close(); } catch (Exception e1) { ; }
-        }
-        */
-
-
-
     }
 }
 
